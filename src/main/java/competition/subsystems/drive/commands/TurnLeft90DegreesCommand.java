41f2832f397933b99a829be24adf9fed195883e1
SHORT-LIFE-CHOSE-ME <118636986+SHORT-LIFE-CHOSE-ME@users.noreply.github.com>
2023-03-18 14:53:55 -0700

    I made progress on the 90 degrees part of the ciriculum

--- src/main/java/competition/subsystems/drive/commands/TurnLeft90DegreesCommand.java
+++ src/main/java/competition/subsystems/drive/commands/TurnLeft90DegreesCommand.java
@@ -2,14 +2,33 @@ package competition.subsystems.drive.commands;
 
 import javax.inject.Inject;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import xbot.common.command.BaseCommand;
 import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.pose.PoseSubsystem;
+
+//import java.lang.annotation.Target;
 
 public class TurnLeft90DegreesCommand extends BaseCommand {
 
     DriveSubsystem drive;
     PoseSubsystem pose;
+
+
+
+    public double Position;
+
+    public double oldPose;
+
+    public double TargetPose;
+
+    public double velocity;
+
+
+
+
+
+
 
     @Inject
     public TurnLeft90DegreesCommand(DriveSubsystem driveSubsystem, PoseSubsystem pose) {
@@ -17,12 +36,40 @@ public class TurnLeft90DegreesCommand extends BaseCommand {
         this.pose = pose;
     }
 
-    @Override
-    public void initialize() {
+    public void initialize(double Position) {
+        pose.getCurrentHeading().getDegrees();
+
     }
+
+    @Override
+    public void initialize(){
+
+    }
+
 
     @Override
     public void execute() {
+
+        double CurrentPose = pose.getCurrentHeading().getDegrees();
+        double error = TargetPose - CurrentPose;
+        double power = -.2922 * error - .2890 * velocity;
+        drive.arcadeDrive(power , -power);
+
+
+
+        oldPose = CurrentPose;
+        velocity = oldPose;
     }
 
-}
+
+    @Override public boolean isFinished()
+    {
+        if (TargetPose < pose.getPosition())
+        {
+            return true;
+        }
+
+        return false;
+
+    }}
+
